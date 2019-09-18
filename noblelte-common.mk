222529d65d82377169edae08a950746e4e640c45
AnanJaser <ananjaser@gmail.com>
2019-09-18 23:03:10 +0400

    zero: override apns/spn configs from samsung
    Imported from heroxlte

--- noblelte-common.mk
+++ noblelte-common.mk
@@ -274,6 +274,11 @@ PRODUCT_PACKAGES += \
 # Properties
 TARGET_SYSTEM_PROP += device/samsung/noblelte-common/system.prop
 
+# APN Definitions - override incomplete/broken lineageos version with Samsung version
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/telephony/apns-conf.xml:system/etc/apns-conf.xml \
+    $(LOCAL_PATH)/configs/telephony/spn-conf.xml:system/etc/spn-conf.xml
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.samsungexynos7420 \
